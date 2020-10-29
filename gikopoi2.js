6860f9a714354f386b42f39cf9ff68cbeea15b75
iccanobif <iccanobif@protonmail.com>
2020-10-29 21:02:49 +0100

    add link to be clicked on when doing yarn start

--- gikopoi2.js
+++ gikopoi2.js
@@ -117,4 +117,4 @@ const port = process.env.PORT == undefined
 
 http.listen(port, "0.0.0.0");
 
-console.log("Server running on port " + port);
+console.log("Server running on http://localhost:" + port);
