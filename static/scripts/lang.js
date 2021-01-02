6d4aecd143707a8e38098baf69424b8e0787ce6a
iccanobif <iccanobif@protonmail.com>
2021-01-02 11:24:01 +0100

    add all room names, fixes #29

--- static/scripts/lang.js
+++ static/scripts/lang.js
@@ -6,17 +6,17 @@ export const messages =
         ui:
         {
             site_name: "BARギコっぽい2",
-            
+
             infobox_label_area: "ｴﾘｱ：",
             infobox_value_area: "海辺の町",
             infobox_label_room: "ｽﾃｰｼﾞ：",
             infobox_label_user_name: "名前：",
             infobox_label_user_count: "人数：",
-            
+
             label_streamer: "ストリーマー：",
             label_input: "入力：",
             label_log: "ログ：",
-            
+
             button_switch_language: "English",
             button_send: "発言",
             button_stream_start: "配信",
@@ -29,9 +29,38 @@ export const messages =
         },
         room:
         {
+            jinja: "神社",
+            jinja_st: "神社入り口",
+            long_st: "鰻道",
+            school_ground: "学校 校庭",
+            bar774: "地下街 バー774",
+            izakaya774: "地下街 居酒屋 名無し",
+            basement: "地下街",
             bar: "バー",
+            school_st: "学校通り",
+            bar_st: "バー通り",
             admin_st: "開発局通り",
-            admin: "開発局"
+            cafe_st: "カフェ通り",
+            busstop: "バス停",
+            idoA: "井戸Ａ",
+            idoB: "井戸Ｂ",
+            school: "学校 教室",
+            admin: "開発局",
+            seashore: "海岸",
+            admin_bar: "秘密のバー",
+            radio: "ラヂヲ局",
+            radio_room1: "ラヂヲ局 第1スタジオ",
+            yatai: "屋台",
+            kaidan: "高台の階段",
+            takadai: "高台",
+            radio_gakuya: "ラヂヲ局 楽屋",
+            radio_backstage: "ラヂヲ局 舞台裏",
+            radio_room2: "ラヂヲ局 第2スタジオ BEAT GIKO",
+            radio_room3: "ラヂヲ局 第3スタジオ G-SQUID",
+            school_rouka: "学校 廊下",
+            school_international: "学校 国際科教室",
+            school_pc: "学校 パソコンルーム",
+            enkai: "宴会場",
         }
     },
     en:
@@ -40,17 +69,17 @@ export const messages =
         ui:
         {
             site_name: "Gikopoi 2",
-            
+
             infobox_label_area: "Area:",
             infobox_value_area: "Sea City",
             infobox_label_room: "Stage:",
             infobox_label_user_name: "Name:",
             infobox_label_user_count: "Users:",
-            
+
             label_streamer: "Streamer:",
             label_input: "Input:",
             label_log: "Log:",
-            
+
             button_switch_language: "日本語",
             button_send: "Send",
             button_stream_start: "Stream",
@@ -63,9 +92,37 @@ export const messages =
         },
         room:
         {
+            long_st: "Eel Track",
+            jinja: "Shrine",
+            jinja_st: "Shrine Entrance",
+            school_ground: "School: Schoolyard",
+            bar774: "Underground Town: Bar 774",
+            izakaya774: "Underground Town: Drunker's Bar",
+            basement: "Underground Town",
             bar: "Bar",
+            school_st: "School Crossing",
+            bar_st: "Bar Street",
             admin_st: "Kanrinin Street",
-            admin: "Developer's Lounge"
+            admin: "Developer's Lounge",
+            admin_bar: "Admin's Bar",
+            cafe_st: "Cafe Vista",
+            enkai: "Banqueting Hall",
+            idoA: "Well A",
+            idoB: "Well B",
+            busstop: "Bus Stop",
+            school: "School: Classroom",
+            school_rouka: "School: Hallway",
+            school_international: "School: Cultural Room",
+            school_pc: "School Computer Lab",
+            seashore: "Seashore",
+            radio: "Radio Studio",
+            radio_room1: "Radio Studio 1",
+            radio_room2: "Radio Studio 2: BEAT GIKO",
+            radio_room3: "Radio Studio 3: G-SQUID",
+            radio_gakuya: "Radio Studio: Dressing Room",
+            radio_backstage: "Radio Studio: Greenroom",
+            yatai: "Sushi Vendor",
+            kaidan: "Hilltop Stairway",
         }
     }
 }
