5cce9569b869e3c6d8af93bc8389bf3a438d32dc
iccanobif <iccanobif@protonmail.com>
2021-01-17 00:32:33 +0100

    fix #69 tabindex=-1 for all links in the log

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -219,7 +219,7 @@ const vueApp = new Vue({
                     .replace(/(https?:\/\/|www\.)[^\s]+/gi, (url, prefix) =>
                 {
                     const href = (prefix == "www." ? "http://" + url : url);
-                    return "<a href='" + href + "' target='_blank'>" + url + "</a>";
+                    return "<a href='" + href + "' target='_blank' tabindex='-1'>" + url + "</a>";
                 });
                 
                 messageDiv.append(authorSpan);
