1d9d6e587278daf37c3c728f1954a069dd954d4d
2sh <git@2sh.me>
2021-02-06 16:19:39 +0000

    Add radio room 1

--- rooms.ts
+++ rooms.ts
@@ -1621,6 +1621,58 @@ export const rooms: { [roomId: string]: Room } = {
         streamSlotCount: 1,
         secret: false
     },
+    radio_room1: {
+        id: "radio_room1",
+        scale: 1,
+        size: { x: 7, y: 8 },
+        originCoordinates: { x: 2, y: 332 },
+        spawnPoint: "right",
+        backgroundImageUrl: "rooms/radio_room1/background.svg",
+        backgroundColor: "#c0c0c0",
+        objects: [
+            { x:  6, y:  3, offset: { x:  381, y:  338 }, url: "table.svg" },
+        ],
+        sit: [
+            { x:  0, y:  0 },
+            { x:  0, y:  1 },
+            { x:  0, y:  2 },
+            { x:  0, y:  3 },
+            { x:  0, y:  4 },
+            { x:  0, y:  5 },
+            { x:  0, y:  6 },
+            { x:  0, y:  7 },
+            
+            { x:  2, y:  0 },
+            { x:  2, y:  1 },
+            { x:  2, y:  2 },
+            { x:  2, y:  3 },
+            { x:  2, y:  4 },
+            { x:  2, y:  5 },
+            { x:  2, y:  6 },
+            { x:  2, y:  7 },
+            
+            { x:  4, y:  0 },
+            { x:  4, y:  1 },
+            { x:  4, y:  2 },
+            { x:  4, y:  3 },
+            { x:  4, y:  4 },
+            { x:  4, y:  5 },
+            { x:  4, y:  6 },
+            { x:  4, y:  7 },
+            
+            { x:  6, y:  2 },
+            { x:  6, y:  4 },
+        ],
+        blocked: [
+            { x:  6, y:  3 },
+        ],
+        forbiddenMovements: [],
+        doors: {
+            right: { x: 6, y: 6, direction: "left", target: "NOT_READY_YET" },
+        },
+        streamSlotCount: 2,
+        secret: false
+    },
     radio_room2: {
         id: "radio_room2",
         scale: 1,
