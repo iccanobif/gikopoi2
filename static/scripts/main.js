26e35b94d00f900b815811bf5713624cad797bc2
iccanobif <iccanobif@protonmail.com>
2021-02-12 21:21:43 +0100

    add console log

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -431,6 +431,7 @@ const vueApp = new Vue({
                 else if(type == "answer")
                 {
                     msg = msg.replace(/\r\n.*candidate.*udp.*\r\n/g, "\r\n");
+                    console.log(msg)
                     rtcPeer.acceptAnswer(msg);
                 }
                 else if(type == "candidate")
