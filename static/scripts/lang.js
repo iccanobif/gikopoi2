82aff5d1d47e1fc30cf9eb30e49d677769ff068e
2sh <git@2sh.me>
2021-02-23 01:19:39 +0000

    Add bubble opacity slider

--- static/scripts/lang.js
+++ static/scripts/lang.js
@@ -77,6 +77,7 @@ export const messages =
             preferences_title: "設定",
             preferences_name_bg: "ｺﾃ背景を表示",
             preferences_shift_enter: "Shift+Enterの改行",
+            preferences_bubble_opacity: "吹き出しの不透明度",
         },
         msg:
         {
@@ -206,6 +207,7 @@ export const messages =
             preferences_title: "Preferences",
             preferences_name_bg: "Show Username Background",
             preferences_shift_enter: "Newline On Shift+Enter",
+            preferences_bubble_opacity: "Speech Bubble Opacity",
         },
         msg:
         {
