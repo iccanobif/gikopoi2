1d72ee6870b2c95c6918c7433d3db66e4644bd86
iccanobif <iccanobif@protonmail.com>
2021-03-08 07:25:01 +0100

    polyfill for Promise.allSettled (fixes #178)

---
+++ static/scripts/polyfills.js
@@ -0,0 +1,11 @@
+Promise.allSettled = Promise.allSettled ||
+    ((promises) => Promise.all(promises.map(p => p
+        .then(v => ({
+            status: 'fulfilled',
+            value: v,
+        }))
+        .catch(e => ({
+            status: 'rejected',
+            reason: e,
+        }))
+    )));
