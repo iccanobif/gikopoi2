54dac0887ec102079add50fcb17af2bed0555db6
iccanobif <iccanobif@protonmail.com>
2021-04-28 00:15:30 +0200

    animalese for japanese (completely random for now)

--- static/scripts/tts.js
+++ static/scripts/tts.js
@@ -1,28 +1,41 @@
 import { urlRegex } from "./utils.js";
 
-const synth = new Animalese('animalese.wav', function() { console.log("test") });
+const synth = new Animalese('animalese.wav', function () { console.log("test") });
 
-function speakAnimalese(text, pitch) {
-  // scale pitch so that it's within the range 0.2 - 2.0
-  pitch =  (pitch/2)*1.8+0.2
-  var audio = new Audio();
-  audio.src = synth.Animalese(text, false, 1).dataURI;
-  audio.play();
+function speakAnimalese(text, pitch)
+{
+    // replace every japanese character with a random roman letter
+    text = text.split("").map(c => isJapaneseCharacter(c) ?
+        ["a", "e", "i", "o", "u"][Math.floor(Math.random() * 5)]
+        : c).join("")
+
+    console.log(text)
+
+    // scale pitch so that it's within the range 0.2 - 2.0
+    pitch = (pitch / 2) * 1.8 + 0.2
+    var audio = new Audio();
+    audio.src = synth.Animalese(text, false, 1).dataURI;
+    audio.play();
+}
+
+function isJapaneseCharacter(character)
+{
+    const charCode = character.charCodeAt(0)
+    // CJK Unified Ideographs 
+    if (charCode >= 0x4E00 && charCode <= 0x9FFF)
+        return true
+    // hiragana + katakana
+    if (charCode >= 0x3040 && charCode <= 0x30FF)
+        return true
 }
 
 function isJapanese(text)
 {
     // very simple heuristic, we just assume that if a sentence has at least one japanese character, then it must be japanese
     for (let i = 0; i < text.length; i++)
-    {
-        const charCode = text.charCodeAt(i)
-        // CJK Unified Ideographs 
-        if (charCode >= 0x4E00 && charCode <= 0x9FFF)
-            return true
-        // hiragana + katakana
-        if (charCode >= 0x3040 && charCode <= 0x30FF)
-            return true
-    }
+        if (isJapaneseCharacter(text.charAt(i))) return true
+    
+    return false
 }
 
 export function speak(message, voiceURI, volume, pitch)
