5d62ccb410a6c2acd12f92f57d6a21cfbc9db8a0
iccanobif <iccanobif@protonmail.com>
2021-06-12 13:40:31 +0200

    chess

--- static/scripts/chessboard-slot.js
+++ static/scripts/chessboard-slot.js
@@ -1,31 +1,62 @@
 Vue.component('chessboard-slot', {
     props: ["socket"],
-    data: function () {
+    template: "#chessboard-slot",
+    data: function ()
+    {
         return {
             chessboard: null
         }
     },
-    mounted: function () {
-        const chessboardElement = document.getElementById("chessboard")
-        this.chessboard = Chessboard(chessboardElement, {
-            pieceTheme: 'chess/img/chesspieces/wikipedia/{piece}.png',
-            position: 'start',
-            draggable: true,
-            onDragStart: (source, piece, position, orientation) => {
-                // TODO
-                // if (game is over or it's the other player's turn)
-                //   return false
-              },
-            onDrop: (source, target) => {
-                this.socket.emit("user-chess-move", source, target);
-              }
-              ,
-            onSnapEnd: () => {
-                // update the board position after the piece snap
-                // for castling, en passant, pawn promotion
-                // this.chessboard.position(game.fen())
-            }
-          })
+    mounted: function ()
+    {
+        this.buildChessboard()
+        this.makeResizable()
     },
-    template: "#chessboard-slot"
+    methods:
+    {
+        buildChessboard: function ()
+        {
+            const chessboardElement = document.getElementById("chessboard")
+
+            this.chessboard = Chessboard(chessboardElement, {
+                pieceTheme: 'chess/img/chesspieces/wikipedia/{piece}.png',
+                position: 'start',
+                draggable: true,
+                onDragStart: (source, piece, position, orientation) =>
+                {
+                    // TODO
+                    // if (game is over or it's the other player's turn)
+                    //   return false
+                },
+                onDrop: (source, target) =>
+                {
+                    this.socket.emit("user-chess-move", source, target);
+                },
+                onSnapEnd: () =>
+                {
+                    // update the board position after the piece snap
+                    // for castling, en passant, pawn promotion
+                    // this.chessboard.position(game.fen())
+                }
+            })
+        },
+        makeResizable: function ()
+        {
+            $("#chessboard").resizable({
+                aspectRatio: true,
+                resize: (event, ui) =>
+                {
+                    this.buildChessboard()
+                },
+                stop: () => 
+                {
+                    // TODO Understand why after the first resize operation the chessboard stops
+                    // being resizable
+                    
+                    // console.log("stop")
+                    // setTimeout(() => this.makeResizable(), 100)
+                }
+            })
+        }
+    },
 })
