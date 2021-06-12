b3068ae3aad2700f3b3d74a92f65e546c461ed81
iccanobif <iccanobif@protonmail.com>
2021-06-12 13:16:00 +0200

    move chessboard component to separate file

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -53,38 +53,6 @@ const i18n = new VueI18n({
     messages,
 });
 
-Vue.component('chessboard-slot', {
-    props: ["socket"],
-    data: function () {
-        return {
-            chessboard: null
-        }
-    },
-    mounted: function () {
-        const chessboardElement = document.getElementById("chessboard")
-        this.chessboard = Chessboard(chessboardElement, {
-            pieceTheme: 'chess/img/chesspieces/wikipedia/{piece}.png',
-            position: 'start',
-            draggable: true,
-            onDragStart: (source, piece, position, orientation) => {
-                // TODO
-                // if (game is over or it's the other player's turn)
-                //   return false
-              },
-            onDrop: (source, target) => {
-                this.socket.emit("user-chess-move", source, target);
-              }
-              ,
-            onSnapEnd: () => {
-                // update the board position after the piece snap
-                // for castling, en passant, pawn promotion
-                // this.chessboard.position(game.fen())
-            }
-          })
-    },
-    template: "#chessboard-slot"
-})
-
 window.vueApp = new Vue({
     i18n,
     el: "#vue-app",
