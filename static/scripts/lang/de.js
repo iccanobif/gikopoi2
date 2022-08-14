a4f95262921f97003c8d06f7baf6840df804e815
2sh <git@2sh.me>
2022-08-15 00:44:08 +0200

    Add DE translations

--- static/scripts/lang/de.js
+++ static/scripts/lang/de.js
@@ -137,8 +137,8 @@ export default
         chess_quit_game: "Spiel verlassen",
 
         visible_only_to_specific_users_stream: "Privater Stream",
-        // double_click_to_move_to_new_tab: "",
-        // double_click_to_toggle_fullscreen: "",
+        double_click_to_move_to_new_tab: "Doppelklicken, um zu einem neuen Tab zu wechseln",
+        double_click_to_toggle_fullscreen: "Doppelklicken, um in den Vollbildmodus zu wechseln",
     },
     msg:
     {
