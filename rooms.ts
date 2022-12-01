79d05f3bbd3a25e4a9b1e4056840b4a6e29dafe5
2sh <git@2sh.me>
2022-12-01 21:11:58 +0100

    Add christmas time konbini

--- rooms.ts
+++ rooms.ts
@@ -3833,123 +3833,130 @@ export const rooms: { [roomId: string]: Room } = {
 
 export const dynamicRooms: DynamicRoom[] = []
 
-const regularKonbini: Room = {
-    id: "konbini",
-    group: "gikopoipoi",
-    scale: 1,
-    size: { x: 9, y: 8 },
-    originCoordinates: { x: 4, y: 332 },
-    spawnPoint: "door",
-    backgroundImageUrl: "rooms/konbini/background.svg",
-    objects: [
-        { x:  1, y: 2, offset: { x: 40, y: 124 }, url: "toilet-wall-left.svg" },
-        { x:  1, y: 4, offset: { x: 166, y: 109 }, url: "toilet-wall-right.svg" },
-        { x:  4, y:  6, width: 5, offset: { x: 400, y: 156 }, url: "register.svg"},
-        { x:  6, y:  2, width: 2, height: 2, offset: { x: 280, y: 340 }, url: "freezer.svg" },
-        { x:  1, y:  1, height: 2, offset: { x: 40, y: 184 }, url: "refrigerator.svg", scale: 1 }, // drinks
-        { x:  3, y:  3, width: 2, height: 3, offset: { x: 160, y: 203 }, url: "foods.svg" }, // snacks
-    ],
-    objectRenderSortMethod: "diagonal_scan",
-    sit: [
-        { x:  0, y:  0 },
-    ],
-    blocked: [
-        // bathroom
-        // { x:  0, y:  0 },
-        // { x:  0, y:  1 },
-        { x:  0, y:  4 },
-        // coffee machine
-        { x:  0, y:  7 },
-        { x:  1, y:  7 },
-        { x:  2, y:  7 },
-        // refrigerator
-        { x:  1, y:  0 },
-        { x:  1, y:  1 },
-        // foods
-        { x:  3, y:  1 }, { x:  4, y:  1 },
-        { x:  3, y:  2 }, { x:  4, y:  2 },
-        { x:  3, y:  3 }, { x:  4, y:  3 },
-        // freezer
-        { x:  6, y:  1 }, { x:  7, y:  1 },
-        { x:  6, y:  2 }, { x:  7, y:  2 },
-        // register
-        { x:  4, y:  6 },
-        { x:  5, y:  6 },
-        { x:  6, y:  6 },
-        { x:  7, y:  6 },
-        { x:  8, y:  6 },
-    ],
-    forbiddenMovements: [
-        { xFrom: 0, yFrom: 2, xTo: 1, yTo: 2 },
-        { xFrom: 1, yFrom: 2, xTo: 0, yTo: 2 },
-    ],
-    doors: {
-        door: { x: 0, y: 6, direction: "right", target: { roomId: "bar_giko_square", doorId: "right" } },
-    },
-    streamSlotCount: 1,
-}
-
-const summerKonbini: Room = {
-    id: "konbini",
-    group: "gikopoipoi",
-    scale: 1,
-    size: { x: 9, y: 8 },
-    originCoordinates: { x: 4, y: 332 },
-    spawnPoint: "door",
-    backgroundImageUrl: "rooms/konbini/background.summer.svg",
-    objects: [
-        { x:  4, y:  6, width: 5, offset: { x: 400, y: 156 }, url: "register.summer.svg"},
-        { x:  1, y:  2, height: 2, offset: { x: 40 + 40, y: 184 - 20 }, url: "refrigerator.summer.svg", scale: 1 }, // drinks
-        { x:  1, y:  4, height: 2, offset: { x: 40 * 4, y: 184 - 60 }, url: "bento.summer.svg", scale: 1 }, // drinks
-        { x:  6, y:  2, width: 2, height: 3, offset: { x: 160 + 120, y: 203 + 60 }, url: "foods.summer.svg" }, // snacks
-        { x:  3, y:  3, width: 2, height: 2, offset: { x: 280 - 80, y: 340 - 80 }, url: "freezer.svg" },
-    ],
-    objectRenderSortMethod: "diagonal_scan",
-    sit: [],
-    blocked: [
-        // bathroom
-        { x:  0, y:  4 },
-        { x:  0, y:  1 },
-        // coffee machine
-        { x:  0, y:  7 },
-        { x:  1, y:  7 },
-        { x:  2, y:  7 },
-        // refrigerator
-        { x:  1, y:  1 },
-        { x:  1, y:  2 },
-        { x:  1, y:  3 },
-        { x:  1, y:  4 },
-        // freezer
-        // { x:  3, y:  1 }, { x:  4, y:  1 },
-        { x:  3, y:  2 }, { x:  4, y:  2 },
-        { x:  3, y:  3 }, { x:  4, y:  3 },
-        // foods
-        { x:  6, y:  1 }, { x:  7, y:  1 },
-        { x:  6, y:  2 }, { x:  7, y:  2 },
-        { x:  6, y:  3 }, { x:  7, y:  3 },
-        // register
-        { x:  4, y:  6 },
-        { x:  5, y:  6 },
-        { x:  6, y:  6 },
-        { x:  7, y:  6 },
-        { x:  8, y:  6 },
-    ],
-    forbiddenMovements: [
-        { xFrom: 0, yFrom: 2, xTo: 1, yTo: 2 },
-        { xFrom: 1, yFrom: 2, xTo: 0, yTo: 2 },
-    ],
-    doors: {
-        door: { x: 0, y: 6, direction: "right", target: { roomId: "bar_giko_square", doorId: "right" } },
-    },
-    streamSlotCount: 1,
-}
-
 dynamicRooms.push({
     roomId: "konbini",
-    subscribedAnnualEvents: ["summer"],
+    subscribedAnnualEvents: ["summer", "christmasTime"],
     build: (currentAnnualEvents: string[]) =>
     {
-        rooms["konbini"] = currentAnnualEvents.includes("summer") ? summerKonbini : regularKonbini
+        if (currentAnnualEvents.includes("summer"))
+        {
+            rooms["konbini"] = {
+                id: "konbini",
+                group: "gikopoipoi",
+                scale: 1,
+                size: { x: 9, y: 8 },
+                originCoordinates: { x: 4, y: 332 },
+                spawnPoint: "door",
+                backgroundImageUrl: "rooms/konbini/background.summer.svg",
+                objects: [
+                    { x:  4, y:  6, width: 5, offset: { x: 400, y: 156 }, url: "register.summer.svg"},
+                    { x:  1, y:  2, height: 2, offset: { x: 40 + 40, y: 184 - 20 }, url: "refrigerator.summer.svg", scale: 1 }, // drinks
+                    { x:  1, y:  4, height: 2, offset: { x: 40 * 4, y: 184 - 60 }, url: "bento.summer.svg", scale: 1 }, // drinks
+                    { x:  6, y:  2, width: 2, height: 3, offset: { x: 160 + 120, y: 203 + 60 }, url: "foods.summer.svg" }, // snacks
+                    { x:  3, y:  3, width: 2, height: 2, offset: { x: 280 - 80, y: 340 - 80 }, url: "freezer.svg" },
+                ],
+                objectRenderSortMethod: "diagonal_scan",
+                sit: [],
+                blocked: [
+                    // bathroom
+                    { x:  0, y:  4 },
+                    { x:  0, y:  1 },
+                    // coffee machine
+                    { x:  0, y:  7 },
+                    { x:  1, y:  7 },
+                    { x:  2, y:  7 },
+                    // refrigerator
+                    { x:  1, y:  1 },
+                    { x:  1, y:  2 },
+                    { x:  1, y:  3 },
+                    { x:  1, y:  4 },
+                    // freezer
+                    // { x:  3, y:  1 }, { x:  4, y:  1 },
+                    { x:  3, y:  2 }, { x:  4, y:  2 },
+                    { x:  3, y:  3 }, { x:  4, y:  3 },
+                    // foods
+                    { x:  6, y:  1 }, { x:  7, y:  1 },
+                    { x:  6, y:  2 }, { x:  7, y:  2 },
+                    { x:  6, y:  3 }, { x:  7, y:  3 },
+                    // register
+                    { x:  4, y:  6 },
+                    { x:  5, y:  6 },
+                    { x:  6, y:  6 },
+                    { x:  7, y:  6 },
+                    { x:  8, y:  6 },
+                ],
+                forbiddenMovements: [
+                    { xFrom: 0, yFrom: 2, xTo: 1, yTo: 2 },
+                    { xFrom: 1, yFrom: 2, xTo: 0, yTo: 2 },
+                ],
+                doors: {
+                    door: { x: 0, y: 6, direction: "right", target: { roomId: "bar_giko_square", doorId: "right" } },
+                },
+                streamSlotCount: 1,
+            }
+        }
+        else
+        {
+            rooms["konbini"] = {
+                id: "konbini",
+                group: "gikopoipoi",
+                scale: 1,
+                size: { x: 9, y: 8 },
+                originCoordinates: { x: 4, y: 332 },
+                spawnPoint: "door",
+                backgroundImageUrl:
+                    currentAnnualEvents.includes("christmasTime") ?
+                    "rooms/konbini/background.christmas.svg" :
+                    "rooms/konbini/background.svg",
+                objects: [
+                    { x:  1, y: 2, offset: { x: 40, y: 124 }, url: "toilet-wall-left.svg" },
+                    { x:  1, y: 4, offset: { x: 166, y: 109 }, url: "toilet-wall-right.svg" },
+                    { x:  4, y:  6, width: 5, offset: { x: 400, y: 156 },
+                        url: currentAnnualEvents.includes("christmasTime") ? "register.christmas.svg" : "register.svg"},
+                    { x:  6, y:  2, width: 2, height: 2, offset: { x: 280, y: 340 }, url: "freezer.svg" },
+                    { x:  1, y:  1, height: 2, offset: { x: 40, y: 184 }, url: "refrigerator.svg", scale: 1 }, // drinks
+                    { x:  3, y:  3, width: 2, height: 3, offset: { x: 160, y: 203 }, url: "foods.svg" }, // snacks
+                ],
+                objectRenderSortMethod: "diagonal_scan",
+                sit: [
+                    { x:  0, y:  0 },
+                ],
+                blocked: [
+                    // bathroom
+                    // { x:  0, y:  0 },
+                    // { x:  0, y:  1 },
+                    { x:  0, y:  4 },
+                    // coffee machine
+                    { x:  0, y:  7 },
+                    { x:  1, y:  7 },
+                    { x:  2, y:  7 },
+                    // refrigerator
+                    { x:  1, y:  0 },
+                    { x:  1, y:  1 },
+                    // foods
+                    { x:  3, y:  1 }, { x:  4, y:  1 },
+                    { x:  3, y:  2 }, { x:  4, y:  2 },
+                    { x:  3, y:  3 }, { x:  4, y:  3 },
+                    // freezer
+                    { x:  6, y:  1 }, { x:  7, y:  1 },
+                    { x:  6, y:  2 }, { x:  7, y:  2 },
+                    // register
+                    { x:  4, y:  6 },
+                    { x:  5, y:  6 },
+                    { x:  6, y:  6 },
+                    { x:  7, y:  6 },
+                    { x:  8, y:  6 },
+                ],
+                forbiddenMovements: [
+                    { xFrom: 0, yFrom: 2, xTo: 1, yTo: 2 },
+                    { xFrom: 1, yFrom: 2, xTo: 0, yTo: 2 },
+                ],
+                doors: {
+                    door: { x: 0, y: 6, direction: "right", target: { roomId: "bar_giko_square", doorId: "right" } },
+                },
+                streamSlotCount: 1,
+            }
+        }
     }
 });
 
