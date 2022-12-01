3de30ad5ed036c522d86b1335f2a08a7bf423254
2sh <git@2sh.me>
2022-12-01 17:27:16 +0100

    Improve formatting

--- annualevents.ts
+++ annualevents.ts
@@ -1,4 +1,4 @@
-import { AnnualEventObject, AnnualEventCallback} from "./types";
+import { AnnualEventObject, AnnualEventCallback } from "./types";
 import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc'
 import timezone from 'dayjs/plugin/timezone'
