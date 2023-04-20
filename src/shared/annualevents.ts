8be86f123de99e8bf88507264379bc3b15e0c8db
2sh <git@2sh.me>
2023-04-20 10:44:34 -0400

    Improve annual events

--- src/shared/annualevents.ts
+++ src/shared/annualevents.ts
@@ -4,12 +4,7 @@ import timezone from 'dayjs/plugin/timezone'
 dayjs.extend(utc)
 dayjs.extend(timezone)
 
-
-export interface AnnualEventObject
-{
-    from: string;
-    to: string;
-}
+type RangeDateDeterminer = (now: dayjs.Dayjs) => dayjs.Dayjs
 
 export type AnnualEventCallback = (currentEvents: string[], addedEvents: string[], removedEvents: string[]) => void
 
@@ -19,74 +14,93 @@ function getNow(): dayjs.Dayjs
     return dayjs().tz("Asia/Tokyo")
 }
 
-function parseEventString(eventString: string): dayjs.Dayjs
+function isSameOrAfter(d1: dayjs.Dayjs, d2: dayjs.Dayjs): boolean
 {
-    return new Function("getNow", "return getNow()." + eventString)(getNow)
+    return !d1.isBefore(d2)
+}
+
+function isSameOrBefore(d1: dayjs.Dayjs, d2: dayjs.Dayjs): boolean
+{
+    return !d1.isAfter(d2)
 }
 
 export class AnnualEvent
 {
-    private from: dayjs.Dayjs;
-    private to: dayjs.Dayjs;
+    private from: RangeDateDeterminer;
+    private to: RangeDateDeterminer;
     private areRangeDatesSameYear: boolean;
     
-    constructor(annualEventObject: AnnualEventObject)
+    constructor(from: RangeDateDeterminer, to: RangeDateDeterminer)
     {
-        this.from = parseEventString("startOf('day')." + annualEventObject.from);
-        this.to = parseEventString("endOf('day')." + annualEventObject.to);
-        this.areRangeDatesSameYear = this.from.isBefore(this.to);
+        this.from = from
+        this.to = to
+        
+        const now = getNow()
+        this.areRangeDatesSameYear = this.determineFrom(now).isBefore(this.determineTo(now));
     }
     
-    getNextEventDate(checkDate?: dayjs.Dayjs): dayjs.Dayjs
+    private determineFrom(checkDate: dayjs.Dayjs): dayjs.Dayjs
+    {
+        return this.from(checkDate.startOf('day'))
+    }
+    
+    private determineTo(checkDate: dayjs.Dayjs): dayjs.Dayjs
+    {
+        return this.to(checkDate.endOf('day'))
+    }
+    
+    public getNextEventDate(checkDate?: dayjs.Dayjs): dayjs.Dayjs
     {
         const cd = checkDate ? checkDate : getNow()
-        if (this.isBetween(cd))
-            return this.to
+        const fromTime = this.determineFrom(cd)
+        const toTime = this.determineTo(cd)
+        
+        const firstInYear = this.areRangeDatesSameYear ? fromTime : toTime
+        const lastInYear = this.areRangeDatesSameYear ? toTime : fromTime
+        
+        if (cd.isBefore(firstInYear))
+            return firstInYear
+        else if (cd.isBefore(lastInYear))
+            return lastInYear
         else
-            return this.from
+            return firstInYear.add(1, 'year')
     }
     
-    isBetween(checkDate: dayjs.Dayjs): boolean
+    public isBetween(checkDate: dayjs.Dayjs): boolean
     {
+        const fromTime = this.determineFrom(checkDate)
+        const toTime = this.determineTo(checkDate)
         if (this.areRangeDatesSameYear)
-        {   // if between the dates
-            return !checkDate.isBefore(this.from) && !checkDate.isAfter(this.to);
-        }
+            return isSameOrAfter(checkDate, fromTime) && isSameOrBefore(checkDate, toTime)
         else
-        {   // if after the `from` date OR before the `to` date
-            return !checkDate.isBefore(this.from) || !checkDate.isAfter(this.to);
-        }
+            return isSameOrBefore(checkDate, toTime) || isSameOrAfter(checkDate, fromTime)
     }
     
-    isNow(): boolean
+    public isNow(): boolean
     {
         const now = getNow()
         return this.isBetween(now)
     }
 }
 
-
-export const annualEventDefinitions: {[eventName: string]: AnnualEventObject} =
+export const annualEvents: {[eventName: string]: AnnualEvent} =
 {
-    spring: {from: "month(2).date(21)", to: "month(4).endOf('month')"}, // starting with cherry blossoms blooming
-    summer: {from: "month(5).startOf('month')", to: "month(7).endOf('month')"}, // sun
-    autumn: {from: "month(8).startOf('month')", to: "month(10).endOf('month')"}, // orange/yellow/brown colors
-    winter: {from: "month(11).startOf('month')", to: "month(2).date(20)"}, // snow
+    spring: new AnnualEvent((now) => now.month(2).date(21), (now) => now.month(4).endOf('month')), // starting with cherry blossoms blooming
+    summer: new AnnualEvent((now) => now.month(5).startOf('month'), (now) => now.month(7).endOf('month')), // sun
+    autumn: new AnnualEvent((now) => now.month(8).startOf('month'), (now) => now.month(10).endOf('month')), // orange/yellow/brown colors
+    winter: new AnnualEvent((now) => now.month(11).startOf('month'), (now) => now.month(2).date(20)), // snow
     
-    sakura: {from: "month(2).date(21)", to: "month(3).endOf('month')"}, // cherry blossoms
-    goldenWeek: {from: "month(3).date(29)", to: "month(4).date(5)"},
-    rainy: {from: "month(5).startOf('month')", to: "month(5).endOf('month')"}, // tsuyu / rainy season
-    fireflies: {from: "month(6).date(1)", to: "month(6).date(9)"}, // ホタル観賞
-    akizakura: {from: "month(8).startOf('month')", to: "month(8).endOf('month')"}, // cosmos flowers
-    spooktober: {from: "month(9).date(17)", to: "month(10).date(1)"},
-    christmasTime: {
-        from: "month(11).date(24).startOf('week').subtract(3, 'week')", // first advent
-        to: "month(11).date(30)"},
-    newYears: {from: "month(11).date(31)", to: "month(0).date(1)"},
+    sakura: new AnnualEvent((now) => now.month(2).date(21), (now) => now.month(3).endOf('month')), // cherry blossoms
+    goldenWeek: new AnnualEvent((now) => now.month(3).date(29), (now) => now.month(4).date(5)),
+    rainy: new AnnualEvent((now) => now.month(5).startOf('month'), (now) => now.month(5).endOf('month')), // tsuyu / rainy season
+    fireflies: new AnnualEvent((now) => now.month(6).date(1), (now) => now.month(6).date(9)), // ホタル観賞
+    akizakura: new AnnualEvent((now) => now.month(8).startOf('month'), (now) => now.month(8).endOf('month')), // cosmos flowers
+    spooktober: new AnnualEvent((now) => now.month(9).date(17), (now) => now.month(10).date(1)),
+    christmasTime: new AnnualEvent(
+        (now) => now.month(11).date(24).startOf('week').subtract(3, 'week'), // first advent
+        (now) => now.month(11).date(30)),
+    newYears: new AnnualEvent((now) => now.month(11).date(31), (now) => now.month(0).date(1)),
 }
-
-export const annualEvents: {[eventName: string]: AnnualEvent} = Object.fromEntries(
-    Object.entries(annualEventDefinitions).map(([eventName, annualEventObject]) => [eventName, new AnnualEvent(annualEventObject)]));
 
 export function getCurrentAnnualEvents(): string[]
 {
