056c040c39f6fc36e7c0b27c73e107cb37098a18
2sh <git@2sh.me>
2023-04-24 03:13:50 -0400

    Convert utils to typescript

--- src/client/utils.ts
+++ src/client/utils.ts
@@ -129,13 +129,327 @@ export function calculateRealCoordinates(room: Room, x: number, y: number): {x: 
     return { x: realX, y: realY }
 }
 
-// further down
+export const sleep = (milliseconds: number) => new Promise(resolve => setTimeout(resolve, milliseconds));
 
-export function logToServer(msg: string)
+export function postJson(url: string, data: any): Promise<any>
+{
+    return fetch(url, {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    })
+}
+
+export function logToServer(msg: string): Promise<any>
 {
     return fetch("/client-log", {
         method: "POST",
         headers: { 'Content-Type': "text/plain"},
         body: msg
     })
+}
+
+
+// Some websites don't seem to realize that URL encoded strings should decode to UTF-8 and not to SHIFT-JIS.
+// example: https://seesaawiki.jp/your_heart/d/%A4%C8%A4%AD%A4%E1%A4%AD%A5%BB%A5%F3%A5%B5%A1%BC%A4%CB%A4%C4%A4%A4%A4%C6
+export function safeDecodeURI(str: string): string
+{
+    try {
+        return decodeURI(str)
+    }
+    catch (exc)
+    {
+        return str
+    }
+}
+
+export const debounceWithDelayedExecution = (func: any, wait: number): ((...args: any) => void) => {
+    let timeout: number;
+  
+    return function executedFunction(...args) {
+      const later = () => {
+        window.clearTimeout(timeout);
+        func(...args);
+      };
+  
+      window.clearTimeout(timeout);
+      timeout = window.setTimeout(later, wait);
+    };
+  };
+
+export const debounceWithImmediateExecution = (func: any, wait: number) => {
+    let lastExecution: number | null = null;
+  
+    return function executedFunction(...args: any) {
+      if (Date.now() - (lastExecution || 0) > wait)
+      {
+        lastExecution = Date.now()
+        func(...args);
+      }
+    };
+  };
+
+const maxGain = 1.3
+
+var AudioContext = window.AudioContext          // Default
+                 || (window as any).webkitAudioContext;  // Safari and old versions of Chrome
+
+export type VuMeterCallback = (level: number) => void
+
+export class AudioProcessor
+{
+    private stream: MediaStream
+    private volume: number = 0
+    private isInbound: boolean
+    private vuMeterCallback: VuMeterCallback
+    
+    public isBoostEnabled: boolean = false // set by v-model
+    private isMute: boolean = false
+    
+    private context: AudioContext
+    private source: MediaStreamAudioSourceNode
+    private destination: MediaStreamAudioDestinationNode
+    private compressor: DynamicsCompressorNode
+    private gain: GainNode
+    private pan: StereoPannerNode | GainNode
+    private analyser: AnalyserNode
+    
+    private vuMeterTimer: number
+    
+    constructor(stream: MediaStream, volume: number, isInbound: boolean, vuMeterCallback: VuMeterCallback)
+    {
+        this.stream = stream
+        this.isInbound = isInbound
+        this.vuMeterCallback = vuMeterCallback
+
+        this.context = new AudioContext();
+        this.source = this.context.createMediaStreamSource(stream);
+        this.destination = this.context.createMediaStreamDestination()
+        this.compressor = this.context.createDynamicsCompressor();
+        this.compressor.threshold.value = -50;
+        this.compressor.knee.value = 40;
+        this.compressor.ratio.value = 12;
+        this.compressor.attack.value = 0;
+        this.compressor.release.value = 0.25;
+        this.gain = this.context.createGain();
+
+        // To support old safari versions that people still seem to use, check that createStereoPanner
+        // is available, and if not, use a dummy gain node instead.
+        if (this.context.createStereoPanner)
+        {
+            this.pan = this.context.createStereoPanner();
+            // @ts-ignore
+            this.pan.value = 0
+        }
+        else
+        {
+            this.pan = this.context.createGain();
+        }
+
+        this.analyser = this.context.createAnalyser()
+
+        this.connectNodes()
+
+        this.setVolume(volume)
+
+        // Vu meter
+        this.analyser.minDecibels = -60;
+        this.analyser.maxDecibels = 0;
+        this.analyser.smoothingTimeConstant = 0.01;
+        // fftSize must be 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, or 32768
+        this.analyser.fftSize = 32
+        const bufferLengthAlt = this.analyser.frequencyBinCount;
+        const dataArrayAlt = new Uint8Array(bufferLengthAlt);
+
+        this.vuMeterTimer = window.setInterval(() => {
+            try {
+                this.analyser.getByteFrequencyData(dataArrayAlt)
+
+                const max = dataArrayAlt.reduce((acc, val) => Math.max(acc, val))
+
+                // Convert level to a linear scale between 0 and 1
+                const linearLevel = max / 255
+
+                // Convert level to a logarithmic scale between 0 and 1
+                const sensitivity = 50
+                const logarithmicLevel = Math.log(linearLevel * sensitivity + 1) / Math.log(sensitivity + 1)
+
+                vuMeterCallback(logarithmicLevel)
+            }
+            catch (exc)
+            {
+                console.error(exc)
+                window.clearInterval(this.vuMeterTimer)
+            }
+        }, 100)
+    }
+
+    dispose(): Promise<void>
+    {
+        for (const track of this.stream.getTracks()) track.stop();
+        window.clearInterval(this.vuMeterTimer)
+        return this.context.close().catch(console.error)
+    }
+
+    connectNodes()
+    {
+        this.source.disconnect()
+        this.compressor.disconnect()
+        this.gain.disconnect()
+        this.pan.disconnect()
+
+        if (this.isBoostEnabled)
+        {
+            this.source.connect(this.compressor)
+            this.compressor.connect(this.gain)
+        }
+        else
+        {
+            this.source.connect(this.gain)
+        }
+
+        this.gain.connect(this.pan)
+
+        if (this.isInbound)
+            this.pan.connect(this.context.destination)
+        
+        this.pan.connect(this.destination)
+        this.pan.connect(this.analyser)
+    }
+
+    setVolume(volume: number)
+    {
+        this.volume = volume
+
+        this.gain.gain.value = this.isBoostEnabled
+            ? volume * maxGain
+            : volume
+    }
+
+    mute()
+    {
+        this.gain.gain.value = 0
+        this.isMute = true
+    }
+
+    unmute()
+    {
+        this.gain.gain.value = this.volume
+        this.isMute = false
+    }
+
+    setPan(value: number)
+    {
+        // Check that this is actually a pan node and not a dummy gain node (see comments in constructor)
+        // @ts-ignore
+        if (this.pan.pan)
+            // @ts-ignore
+            this.pan.pan.value = value
+    }
+
+    onCompressionChanged()
+    {
+        this.connectNodes()
+    }
+}
+
+export function getFormattedCurrentDate(): string {
+    const date = new Date()
+    return [date.getFullYear(),
+            "-",
+            (date.getMonth() + 1).toString().padStart(2, '0'),
+            "-",
+            date.getDate().toString().padStart(2, '0'),
+            " ",
+            date.getHours().toString().padStart(2, '0'),
+            ":",
+            date.getMinutes().toString().padStart(2, '0'),
+            ":",
+            date.getSeconds().toString().padStart(2, '0'),
+           ].join('');
+  };
+
+// On normal god-fearing browsers requestPermission() returns a Promise, while
+// safari uses a callback parameter.
+export function requestNotificationPermission(): Promise<string>
+{
+    return new Promise((resolve, reject) => {
+        const promise = Notification.requestPermission(resolve)
+        if (promise)
+            promise.then(resolve)
+    })
+}
+
+export type DeviceInfo = {
+    id: string
+    name: string
+    type: MediaDeviceKind
+}
+
+export async function getDeviceList(includeAudioDevices: boolean, includeVideoDevices: boolean): Promise<DeviceInfo[]>
+{
+    if (!includeVideoDevices && !includeAudioDevices)
+        return []
+
+    // In order to get the labels, we need to get permission first 
+    const mediaStream = await navigator.mediaDevices.getUserMedia({ video: includeVideoDevices, audio: includeAudioDevices })
+
+    const devices = await navigator.mediaDevices.enumerateDevices();
+
+    const output = devices
+        .filter(d => (includeAudioDevices && d.kind == "audioinput") || (includeVideoDevices && d.kind == "videoinput"))
+        .map(d => ({
+            id: d.deviceId,
+            name: d.label,
+            type: d.kind
+        }))
+
+    for (const track of mediaStream.getTracks()) track.stop();
+
+    return output
+}
+
+export function getClickCoordinatesWithinCanvas(canvas: HTMLCanvasElement, clickEvent: MouseEvent, devicePixelRatio: number)
+{
+    const canvasBoundingClientRect = canvas.getBoundingClientRect()
+    
+    return {
+        x: (clickEvent.clientX - canvasBoundingClientRect.x) * devicePixelRatio,
+        y: (clickEvent.clientY - canvasBoundingClientRect.y) * devicePixelRatio,
+    }
+}
+
+// HTML control functions
+
+export const controlCharLT = String.fromCharCode(2) // <
+export const controlCharGT = String.fromCharCode(3) // >
+export const controlCharAmp = String.fromCharCode(31) // &
+
+export function htmlToControlChars(str: string): string
+{
+    return str
+        .replaceAll("&", controlCharAmp)
+        .replaceAll("<", controlCharLT)
+        .replaceAll(">", controlCharGT)
+}
+
+export function controlCharsToHtml(str: string): string
+{
+    return str
+        .replaceAll(controlCharAmp, "&")
+        .replaceAll(controlCharLT, "<")
+        .replaceAll(controlCharGT, ">")
+}
+
+export function removeControlChars(str: string): string
+{
+    return str.replace(/[\u0000-\u001F\u007F-\u009F]/g, "")
+}
+
+export function escapeHTML(str: string): string
+{
+    return str
+        .replaceAll('&', '&amp;')
+        .replaceAll('<', '&lt;')
+        .replaceAll('>', '&gt;')
 }
