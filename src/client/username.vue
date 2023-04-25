2ab38be9b37fc6ab006019c6020863c38674dd8d
2sh <git@2sh.me>
2023-04-25 10:49:21 -0400

    Add watch for user id or name prop changes

--- src/client/username.vue
+++ src/client/username.vue
@@ -17,34 +17,39 @@ const highlightUser = inject('highlightUser') as any
 const ignoredUserIds = inject('ignoredUserIds') as Ref<IgnoredUserIds>
 
 const properUserName = ref(fallbackUserName)
-if (props.userName)
+
+watch(props, () =>
 {
-    properUserName.value = props.userName
-}
-else
-{
-    let unwatch: WatchStopHandle
-    let unwatchTimer: number
-    // watches users until properUserName is set or 4 seconds are over
-    const clearUnwatchTimer = () => window.clearTimeout(unwatchTimer)
-    const checkUsers = (): boolean =>
+    properUserName.value = fallbackUserName
+    if (props.userName)
     {
-        if (!users.value[props.userId]) return true
-        properUserName.value = users.value[props.userId].name
-        if (unwatch)
+        properUserName.value = props.userName
+    }
+    else
+    {
+        let unwatch: WatchStopHandle
+        let unwatchTimer: number
+        // watches users until properUserName is set or 4 seconds are over
+        const clearUnwatchTimer = () => window.clearTimeout(unwatchTimer)
+        const checkUsers = (): boolean =>
         {
-            unwatch()
-            clearUnwatchTimer()
+            if (!users.value[props.userId]) return true
+            properUserName.value = users.value[props.userId].name
+            if (unwatch)
+            {
+                unwatch()
+                clearUnwatchTimer()
+            }
+            return false
         }
-        return false
+        if (checkUsers())
+        {
+            unwatch = watch(users, checkUsers)
+            unwatchTimer = window.setTimeout(unwatch, 4000)
+            onUnmounted(clearUnwatchTimer)
+        }
     }
-    if (checkUsers())
-    {
-        unwatch = watch(users, checkUsers)
-        unwatchTimer = window.setTimeout(unwatch, 4000)
-        onUnmounted(clearUnwatchTimer)
-    }
-}
+}, { deep: true, immediate: true })
 
 const userName = computed(() => ignoredUserIds.value.has(props.userId) ? fallbackUserName : properUserName.value)
 </script>
