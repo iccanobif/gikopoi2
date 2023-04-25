2b2e09b929fa2b510367732a77b13cadb560518a
2sh <git@2sh.me>
2023-04-25 10:21:44 -0400

    Improve username component

--- src/client/username.vue
+++ src/client/username.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { inject, watch, ref, computed } from 'vue'
-import type { Ref } from 'vue'
+import { inject, watch, onUnmounted, ref, computed } from 'vue'
+import type { Ref, WatchStopHandle } from 'vue'
 
 import type { Users, IgnoredUserIds } from './types'
 
@@ -16,21 +16,41 @@ const highlightedUserId = inject('highlightedUserId') as Ref<string>
 const highlightUser = inject('highlightUser') as any
 const ignoredUserIds = inject('ignoredUserIds') as Ref<IgnoredUserIds>
 
-const properUserName = props.userName
-    || users.value[props.userId].name
-    || fallbackUserName
-
-const userName = ref(fallbackUserName)
-watch(ignoredUserIds, () =>
+const properUserName = ref(fallbackUserName)
+if (props.userName)
 {
-    userName.value = ignoredUserIds.value.has(props.userId)
-        ? fallbackUserName
-        : properUserName
-}, { immediate: true, deep: true })
+    properUserName.value = props.userName
+}
+else
+{
+    let unwatch: WatchStopHandle
+    let unwatchTimer: number
+    // watches users until properUserName is set or 4 seconds are over
+    const clearUnwatchTimer = () => window.clearTimeout(unwatchTimer)
+    const checkUsers = (): boolean =>
+    {
+        if (!users.value[props.userId]) return true
+        properUserName.value = users.value[props.userId].name
+        if (unwatch)
+        {
+            unwatch()
+            clearUnwatchTimer()
+        }
+        return false
+    }
+    if (checkUsers())
+    {
+        unwatch = watch(users, checkUsers)
+        unwatchTimer = window.setTimeout(unwatch, 4000)
+        onUnmounted(clearUnwatchTimer)
+    }
+}
+
+const userName = computed(() => ignoredUserIds.value.has(props.userId) ? fallbackUserName : properUserName.value)
 </script>
 
 <template>
     <span class="username"
         :class="{'highlighted-username': props.userId && props.userId == highlightedUserId}"
-        @click="highlightUser(userId, userName)">{{ userName }}</span>
+        @click="highlightUser(props.userId, userName)">{{ userName }}</span>
 </template>
