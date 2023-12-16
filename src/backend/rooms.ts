80356adc874da45530bcf1ab03b04967aa9d708d
iccanobif <iccanobif@protonmail.com>
2023-12-16 23:07:39 +0100

    fix tile that shouldn't have been walkable

--- src/backend/rooms.ts
+++ src/backend/rooms.ts
@@ -3674,7 +3674,7 @@ export const rooms: { [roomId: string]: Room } = {
             .concat(coordRange({x:  0, y:  3}, {x:  4, y:  3}))
             .concat(coordRange({x:  4, y:  2}, {x:  5, y:  2}))
             .concat(coordRange({x:  6, y:  0}, {x:  7, y:  0}))
-            .concat(coordRange({x:  4, y: 12}, {x:  4, y: 13}))
+            .concat(coordRange({x:  4, y: 11}, {x:  4, y: 13}))
 
             //stage
             .concat(coordRange({x:  9, y: 11}, {x: 27, y: 11}))
