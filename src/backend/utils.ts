6f6852707c265db623ea43ffc186d73d6da97cf9
iccanobif <iccanobif@protonmail.com>
2025-01-24 23:18:15 +0100

    fix build error, remove dead code

--- src/backend/utils.ts
+++ src/backend/utils.ts
@@ -4,24 +4,6 @@ export async function sleep(ms: number) {
     return new Promise(res => setTimeout(res, ms))
 }
 
-export function indexOfMulti(array: Uint8Array, searchElements: number[], fromIndex: number): number {
-    fromIndex = fromIndex || 0;
-
-    const index = Array.prototype.indexOf.call(array, searchElements[0], fromIndex);
-    if (searchElements.length === 1 || index === -1) {
-        // Not found or no other elements to check
-        return index;
-    }
-
-    for (const i = index, j = 0; j < searchElements.length && i < array.length; i++, j++) {
-        if (array[i] !== searchElements[j]) {
-            return indexOfMulti(array, searchElements, index + 1);
-        }
-    }
-
-    return (i === index + searchElements.length) ? index : -1;
-};
-
 // Expect key to start with # or ##
 function calculateTripcode(key: string) {
     // for gikopoi compatibility, empty keys return ◆fnkquv7jY2 instead of ◆8NBuQ4l6uQ
