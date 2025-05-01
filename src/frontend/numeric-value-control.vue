336e65eb499314653bf7f1054fbed93f125e7311
iccanobif <iccanobif@protonmail.com>
2025-05-01 12:36:52 +0200

    remove long press on numeric-value-control since it was buggy on mobile

--- src/frontend/numeric-value-control.vue
+++ src/frontend/numeric-value-control.vue
@@ -1,11 +1,6 @@
-<!-- When using ev.preventDefault(), On PC the buttons raise the mousedown and mouseup events,
- while on mobile they raise the touchstart and touchend events  -->
-
-<!-- It's still not working on android. why? -->
-
 <script setup lang="ts">
 import { ref } from 'vue'
-import { defineEmits, onMounted, onBeforeUnmount } from 'vue'
+import { defineEmits } from 'vue'
 
 const props = defineProps({
     min: {
@@ -45,89 +40,15 @@ const reset = () => {
     count.value = props.default
     emitValueChanged()
 }
-
-const intervalMilliseconds = 200
-
-let interval: NodeJS.Timer | null = null
-
-const startIncrement = (event: Event) => {
-    event.preventDefault();
-    if (interval) return
-    increment()
-    interval = setInterval(increment, intervalMilliseconds)
-}
-const stopIncrement = (event: Event) => {
-    event.preventDefault();
-    if (interval) {
-        clearInterval(interval)
-        interval = null
-    }
-}
-
-const startDecrement = (event: Event) => {
-    event.preventDefault();
-    if (interval) return
-    decrement()
-    interval = setInterval(decrement, intervalMilliseconds)
-}
-
-const stopDecrement = (event: Event) => {
-    event.preventDefault();
-    if (interval) {
-        clearInterval(interval)
-        interval = null
-    }
-}
-
-const handleKeyDown = (event: KeyboardEvent, type: "increment" | "decrement") => {
-    event.preventDefault();
-    if (event.key === 'Enter' || event.key === ' ')
-        type === 'increment' ? startIncrement(event) : startDecrement(event)
-}
-
-const handleKeyUp = (event: KeyboardEvent, type: "increment" | "decrement") => {
-    event.preventDefault();
-    type === 'increment' ? stopIncrement(event) : stopDecrement(event)
-}
-
-// TODO: handle touch events (touchstart, touchend, touchcancel)
-
-onMounted(() => {
-    window.addEventListener('mouseup', stopIncrement)
-    window.addEventListener('mouseup', stopDecrement)
-    window.addEventListener('keyup', stopDecrement)
-    window.addEventListener('keyup', stopIncrement)
-    window.addEventListener('touchend', stopIncrement)
-    window.addEventListener('touchend', stopDecrement)
-})
-
-onBeforeUnmount(() => {
-    window.removeEventListener('mouseup', stopIncrement)
-    window.removeEventListener('mouseup', stopDecrement)
-    window.removeEventListener('keyup', stopDecrement)
-    window.removeEventListener('keyup', stopIncrement)
-    window.removeEventListener('keyup', stopDecrement)
-    window.removeEventListener('keyup', stopIncrement)
-})
 </script>
 
 <template>
     <div class="numeric-value-control">
         <button
-            @mousedown="event => startDecrement(event)"
-            @mouseup="event => stopDecrement(event)"
-            @keydown="event => handleKeyDown(event, 'decrement')"
-            @keyup="event => handleKeyUp(event, 'decrement')"
-            @touchstart="event => startDecrement(event)"
-            @touchend="event => stopDecrement(event)"
+            @click="decrement"
             :class="{ disabled: count === props.min, decrement: true }"></button>
         <button
-            @mousedown="event => startIncrement(event)"
-            @mouseup="event => stopIncrement(event)"
-            @keydown="event => handleKeyDown(event, 'increment')"
-            @keyup="event => handleKeyUp(event, 'increment')"
-            @touchstart="event => startIncrement(event)"
-            @touchend="event => stopIncrement(event)"
+            @click="increment"
             :class="{ disabled: count === props.max, increment: true }"></button>
         <button
             class="value"
